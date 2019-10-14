ddc6661e3b0f198781d5018e05fcbfbc00418239
zSteinO34 <zstein1234@gmail.com>
2019-10-14 09:43:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(arr, string) {
+    let matching = arr.filter(str => {
+        return str.toLowerCase() == string.toLowerCase()
+    });
+    return matching;
+}
+
+function fuzzyMatch(arr, str) {
+    let lengthOfName = str.length;
+    let match = arr.filter(string => {
+        return string.slice(0, lengthOfName) === str
+    });
+    return match;
+}
+
+function matchName(arr, str) {
+    return arr.filter(driver => {
+        return driver.name.toLowerCase() === str.toLowerCase();
+    });
+}
